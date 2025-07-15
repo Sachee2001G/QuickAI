27af7f76fd1b6bdd55c75e44f121a14ac0a9f704
Sachee <sacheeghimire@gmail.com>
2025-07-15 21:35:18 +0545

    Adding Routes in App.jsx and Updating main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,50 @@
 //Using snippet for extention ES7/React
+// import React from 'react'
+// import { Routes, Route} from 'react-router-dom'
+// import Home from './assets/pages/Home.jsx'
+// import Community from './assets/pages/Community.jsx'
+// import BlogTitles from './assets/pages/BlogTitles.jsx'
+// import Layout from './assets/pages/Layout'
+// import Dashboard from './assets/pages/Dashboard'
+// import WriteArticle from './assets/pages/WriteArticle'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path='/' element={<Home />} />
+//         <Route path='/ai' element={<Layout />} />
+//           <Route index element={<Dashboard />} />
+//           <Route path ='write-article' element={<WriteArticle />} />
+//           <Route path ='blog-titles' element={<BlogTitles />} />
+//         </Route>
+//       </Routes>
+//     </div >
+  
+//   )
+// }
+
+// export default App
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './assets/pages/Home.jsx'
+import BlogTitles from './assets/pages/BlogTitles.jsx'
+import Layout from './assets/pages/Layout'
+import Dashboard from './assets/pages/Dashboard'
+import WriteArticle from './assets/pages/WriteArticle'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
