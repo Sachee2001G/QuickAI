e367af4a2070dbe136c090fbfc400ca34d13e025
Sachee <sacheeghimire@gmail.com>
2025-07-16 18:25:39 +0545

    Adding routes to the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,6 +32,11 @@ import BlogTitles from './assets/pages/BlogTitles.jsx'
 import Layout from './assets/pages/Layout'
 import Dashboard from './assets/pages/Dashboard'
 import WriteArticle from './assets/pages/WriteArticle'
+import GenerateImages from './assets/pages/GenerateImages'
+import RemoveBackground from './assets/pages/RemoveBackground'
+import RemoveObject from './assets/pages/RemoveObject'
+import ReviewResume from './assets/pages/ReviewResume'
+import Community from './assets/pages/Community.jsx'
 
 const App = () => {
   return (
@@ -42,6 +47,11 @@ const App = () => {
           <Route index element={<Dashboard />} />
           <Route path='write-article' element={<WriteArticle />} />
           <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-objects' element={<RemoveObject />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='community' element={<Community />} />
         </Route>
       </Routes>
     </div>
